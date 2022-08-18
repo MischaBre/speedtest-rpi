c0a3d7a21dc0d3f24c51fbaab3e32702e91cf946
MischaGe <gemsa.michael@gmail.com>
2022-08-18 10:34:28 +0200

    v 0.5 - added documentation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-##############################
+##################################
 # speedtest
 #   by Michael Gemsa
 #   22-08-16
@@ -8,25 +8,42 @@
 #   mysql-connector-python  8.0.30
 #   speedtest-cli           2.1.3
 #
-
+# description:
+#   is a script to be run via cronjob for periodically testing the
+#   speed of the internet connection.
+#   You need a mysql database. The file mysql.cfg specifies the connection details to your database.
+#   the script runs, makes a speedtest with speedtest.net, returns download speed
+#   and upload speed in MBIT/s and adds the results to your database.
+#   when a speedtest-error occurs it tries to add an empty element the error message including.
+#
+#
+#
+#   setup:
+#   run the script with the parameter --setup to initialize the table in your database.
+#   your need a sql-user with create table and insert privileges.
+#
+#   creates a table with:
+#       id int, datum datetime, ip varchar, downspeed decimal, upspeed decimal, isp varchar, error varchar
+#
+##################################
 
 ### IMPORTS
 import logging
 import sys
-
 import speedtest
 import mysql.connector
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
-### GLOBALS
+########################### GLOBALS
+# SPEEDTEST_GLOBALS
 CONVERT_TO_MBIT = 1_000_000
 MAX_SERVER = 5
 
-#
+# LOG_GLOBALS
 LOG_LEVEL = logging.INFO
 LOG_FILE = 'speedtest.log'
-LOG_MAXBYTES = 1*1024*1024
+LOG_MAXBYTES = 1 * 1024 * 1024
 LOG_BACKUPCOUNT = 1
 
 # define MYSQL GLOBALS from mysql.cfg
@@ -38,19 +55,21 @@ DB_HOST = DB_FILELINES[2].split("=")[1].rstrip('\n')
 DB_DB = DB_FILELINES[3].split("=")[1].rstrip('\n')
 DB_TABLE = DB_FILELINES[4].split("=")[1].rstrip('\n')
 
-DB_INSERT = ('insert into {} '
+# SQL QUERIES
+DB_Q_INSERT = ('insert into {} '
              '(datum, ip, isp, downspeed, upspeed, error) '
              'values (%s, %s, %s, %s, %s, %s)').format(DB_TABLE)
-DB_SETUP = ('use {}').format(DB_DB)
-DB_SETUP2 = ('create table {} ('
-            'id int auto_increment,'
-            'datum datetime not null,'
-            'ip varchar(15) not null,'
-            'downspeed decimal(5,2) not null,'
-            'upspeed decimal(5,2) not null,'
-            'isp varchar(50) not null,'
-            'error varchar(100),'
-            'primary key (id))').format(DB_TABLE)
+DB_Q_SETUP = ('use {}').format(DB_DB)
+DB_Q_SETUP2 = ('create table {} ('
+             'id int auto_increment,'
+             'datum datetime not null,'
+             'ip varchar(15) not null,'
+             'downspeed decimal(5,2) not null,'
+             'upspeed decimal(5,2) not null,'
+             'isp varchar(50) not null,'
+             'error varchar(100),'
+             'primary key (id))').format(DB_TABLE)
+
 
 ### FUNCTIONS
 
@@ -94,14 +113,18 @@ def speedTest() -> (str, str, float, float):
 # raises:   mysql.connector.Error
 def createCursor():
     try:
+        # create mysql.connection and cursor with GLOBALS
         cn = mysql.connector.connect(user=DB_USER, password=DB_PW, host=DB_HOST, database=DB_DB)
         cur = cn.cursor()
     except mysql.connector.Error as err:
+        # raise mysql-error to main()
         raise err
     return cn, cur
 
 
 ### setupDB
+# creates mysql-cursor for mysql operations. creates table as in DB_Q_SETUP defined
+# return:   int     0: ran perfectly,   2: mysql-error
 def setupDB() -> int:
     cnx = None
     cursor = None
@@ -111,18 +134,27 @@ def setupDB() -> int:
         logger.info('cursor created')
 
         # use DB_TABLE;
-        cursor.execute(DB_SETUP)
+        cursor.execute(DB_Q_SETUP)
 
         # create table DB_TABLE ( ... )
-        cursor.execute(DB_SETUP2)
+        cursor.execute(DB_Q_SETUP2)
         logger.info('tables created')
 
+        # commit
         cnx.commit()
         logger.info('committed')
     except mysql.connector.Error as err:
         logger.error('Fehler! {}'.format(err))
         return 2
+    finally:
+        # close all possible sql connection
+        if cursor:
+            cursor.close()
+        if cnx:
+            cnx.close()
+        logger.info('db closed')
     return 0
+
 
 ### insertData
 # insertData takes mysql-cursor. Inserts ip, down, up in table lm_speedtest.
@@ -130,7 +162,7 @@ def setupDB() -> int:
 # Catches any mysql error.
 # attr:     ip (String), down (double), up (double)
 # return:   void
-def insertData(ip, isp, down, up, error = '') -> None:
+def insertData(ip, isp, down, up, error='') -> None:
     cnx = None
     cursor = None
     try:
@@ -140,7 +172,7 @@ def insertData(ip, isp, down, up, error = '') -> None:
 
         # execute INSERT...
         insertValues = (datetime.now(), ip, isp, down, up, error)
-        cursor.execute(DB_INSERT, insertValues)
+        cursor.execute(DB_Q_INSERT, insertValues)
         logger.info('data inserted')
 
         # commit
@@ -148,6 +180,7 @@ def insertData(ip, isp, down, up, error = '') -> None:
         logger.info('committed')
 
     except mysql.connector.Error as err:
+        # raise mysql-error to main()
         raise err
 
     finally:
@@ -160,6 +193,7 @@ def insertData(ip, isp, down, up, error = '') -> None:
 
 
 ### main
+# return:   int     0: ran perfectly,   1: speedtest-error,     2: mysql-error
 def main() -> int:
     try:
         # try speedtest
@@ -171,22 +205,27 @@ def main() -> int:
     except speedtest.ConfigRetrievalError as err:
         logger.error('Speedtest-Fehler! {}'.format(err))
         try:
+            # insert empty data with error message
             insertData('0.0.0.0', '-', 0.0, 0.0, str(err))
         except mysql.connector.Error as db_err:
-            logger.error("Fehler! {}".format(db_err))
-            return 1
+            # couldnt insert empty data
+            logger.error("MySQL-Fehler! {}".format(db_err))
+            return 2
         return 1
     except ValueError as err:
         logger.error('Speedtest-Fehler! {}'.format(err))
         try:
+            # insert empty data with error message
             insertData('0.0.0.0', '-', 0.0, 0.0, str(err))
         except mysql.connector.Error as db_err:
-            logger.error("Fehler! {}".format(db_err))
-            return 1
+            # couldnt insert empty data
+            logger.error("MySQL-Fehler! {}".format(db_err))
+            return 2
         return 1
     except mysql.connector.Error as db_err:
-        logger.error("Fehler! {}".format(db_err))
-        return 1
+        # couldnt insert speedtest data
+        logger.error("MySQL-Fehler! {}".format(db_err))
+        return 2
 
 
 ### PROGRAM START
@@ -194,7 +233,7 @@ if __name__ == '__main__':
     # create global logger
     logger, rotLog = createLogger()
     args = sys.argv
-    if args and args[0] == 'setup':
+    if args and args[0] == '--setup':
         sys.exit(setupDB())
     else:
         sys.exit(main())
